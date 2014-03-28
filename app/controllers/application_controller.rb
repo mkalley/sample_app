b1cac2b5f4f5026fb0757bece783f14e7f63358f
Mick Alley <alleymick@gmail.com>
2014-03-28 15:18:07 -0500

    Finish user edit update index and destroy actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :signed_in_user, only: [:index, :edit, :update]
+
+  def index
+    @users = User.all
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
